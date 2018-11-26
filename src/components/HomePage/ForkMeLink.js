cca49a670b835b200ee46e9ae657a72c84166a7e
Egor Piskunov <egor.piskunou@gmail.com>
2018-11-26 17:22:02 +0300

    Extract ForkMeLink from HomePage

---
+++ src/components/HomePage/ForkMeLink.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ForkMeLink = () => (
+  <a href="https://github.com/you">
+    <img
+      className="forkme"
+      src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+      alt="Fork me on GitHub"
+    />
+  </a>
+)
+
+export default ForkMeLink
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Button, Layout } from 'antd'
 import { dispatch } from 'store'
 import { startGame } from 'ducks/gameboard'
+import ForkMeLink from './ForkMeLink'
 import logo from 'assets/collage.svg'
 import './HomePage.css'
 
@@ -19,13 +20,7 @@ class HomePage extends Component {
     return (
       <Layout style={{ height: '-webkit-fill-available' }}>
         <Content className="home">
-          <a href="https://github.com/you">
-            <img
-              className="forkme"
-              src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
-              alt="Fork me on GitHub"
-            />
-          </a>
+          <ForkMeLink />
           <img src={logo} alt="logo" />
           <h1>Memory Game</h1>
           <Button size="large" onClick={this.handleStartGameButton}>
